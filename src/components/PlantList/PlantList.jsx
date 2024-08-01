981a2fd647b20c118e8c4e731a7c7253d539b61c
BaileyBeck <baileybk2@gmail.com>
2024-07-31 21:56:57 -0400

    added basic SASS styles to NavBar, forms, and containers

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -2,16 +2,18 @@ import { Link } from "react-router-dom"
 
 const PlantList = (props) => {
   return (
-    <main>
+    <>
       <h1>Your Plants</h1>
-      {props.plants.map((plant, index) => (
-        <Link key={index} to={`/plants/${plant._id}`}>
-          <h2>
-            {plant.name} <img src={plant.img} alt={plant.name} />
-          </h2>
-        </Link>
-      ))}
-    </main>
+      <main className="container">
+        {props.plants.map((plant, index) => (
+          <Link key={index} to={`/plants/${plant._id}`}>
+            <h2>
+              {plant.name} <img src={plant.img} alt={plant.name} />
+            </h2>
+          </Link>
+        ))}
+      </main>
+    </>
   )
 }
 
